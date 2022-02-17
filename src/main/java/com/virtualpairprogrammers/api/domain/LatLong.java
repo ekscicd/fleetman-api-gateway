d514ee368250fc2002011553562b9f32a97af048
SHIVA <44166868+Epic-Shiv@users.noreply.github.com>
2022-02-17 15:36:23 +0530

    Update LatLong.java

--- src/main/java/com/virtualpairprogrammers/api/domain/LatLong.java
+++ src/main/java/com/virtualpairprogrammers/api/domain/LatLong.java
@@ -2,9 +2,9 @@ package com.virtualpairprogrammers.api.domain;
 
 import java.math.BigDecimal;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
+//import javax.xml.bind.annotation.XmlAccessType;
+//import javax.xml.bind.annotation.XmlAccessorType;
+//import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
